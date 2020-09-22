7bd0831b786561695bfffcd23ac7d85962069cf4
ivy-1996 <919624032@qq.com>
2020-09-22 20:13:30 +0800

    add template view

--- app.go
+++ app.go
@@ -7,21 +7,36 @@ import (
 	"strings"
 )
 
+func handleError(r view.RestViewer, ctx *gin.Context) {
+	err := recover()
+	r.HandleError(ctx, err)
+}
+
+// make your RestView to become a gin.HandlerFunc
 func AsHandlerFunc(r view.RestViewer) gin.HandlerFunc {
 
+	// get reflect view value
+	// it will finish before this application start
+	// don't care about consuming too much performance
 	value := reflect.ValueOf(r)
 
 	return func(ctx *gin.Context) {
 
-		defer func() {
-			err := recover()
-			r.HandleError(ctx, err)
-		}()
+		defer handleError(r, ctx)
 
 		if r.IsAllowRequest(ctx) {
+
+			// Make request method from upper to title
+			// GET -> get -> Get
 			method := strings.Title(strings.ToLower(ctx.Request.Method))
+
+			// try to find your handle in your view
 			handle := value.MethodByName(method)
+
+			// if we find
+			// check if valid
 			if handle.IsValid() {
+
 				r.PrePare(ctx)
 				context := reflect.ValueOf(ctx)
 				handle.Call([]reflect.Value{context})
@@ -29,8 +44,19 @@ func AsHandlerFunc(r view.RestViewer) gin.HandlerFunc {
 				return
 			}
 		} else {
+			// handle forbidden request
 			view.ForbiddenHandleFunc(ctx)
 		}
+		// if request is allowed
+		// but don't have any handle to call
 		view.MethodNotAllowedHandleFunc(ctx)
 	}
+}
+
+// RenderTemplateView
+// shortcut for template render
+func RenderTemplateView(ctx *gin.Context, t view.TemplateViewer) {
+	template := t.GetTemplateName(ctx)
+	context := t.GetTemplateContext(ctx)
+	ctx.HTML(200, template, context)
 }
--- view/view.go
+++ view/view.go
@@ -10,10 +10,12 @@ var (
 	forbiddenResponseBody        = []byte("403 Forbidden")
 )
 
+// Change default MethodNotAllowed tips
 func SetMethodNotAllowedBody(body []byte) {
 	methodNotAllowedResponseBody = body
 }
 
+// Change default Forbidden tips
 func SetForbiddenResponseBody(body []byte) {
 	forbiddenResponseBody = body
 }
@@ -26,28 +28,72 @@ func ForbiddenHandleFunc(ctx *gin.Context) {
 	ctx.String(http.StatusForbidden, string(forbiddenResponseBody))
 }
 
+// Abstract RestViewer
 type RestViewer interface {
-	// before call your handle
+	// Before call your handle
 	PrePare(ctx *gin.Context)
 
-	// after call your handle
+	// After call your handle
 	Finish(ctx *gin.Context)
 
+	// Continue precess if return true
+	// ForbiddenHandleFunc will be called if return false
 	IsAllowRequest(ctx *gin.Context) bool
 
+	// Implement this method to handle panic error
 	HandleError(ctx *gin.Context, err interface{})
 }
 
+// Base RestViewer implement
+// All RestView must extend this struct
 type RestView struct{}
 
+// Implement RestViewer
+// Overwrite to your own idea
 func (b *RestView) PrePare(ctx *gin.Context) {}
 
+// Implement RestViewer
+// Overwrite to your own idea
 func (b *RestView) Finish(ctx *gin.Context) {}
 
+// Implement RestViewer
+// Default always return true
+// Overwrite to your own idea
 func (b *RestView) IsAllowRequest(ctx *gin.Context) bool {
 	return true
 }
 
+// Implement RestViewer
+// Default panic
+// Overwrite to your own idea
 func (b *RestView) HandleError(ctx *gin.Context, err interface{}) {
 	panic(err)
+}
+
+
+
+type TemplateViewer interface {
+	RestViewer
+	GetTemplateName(ctx *gin.Context) string
+	GetTemplateContext(ctx *gin.Context) interface{}
+}
+
+// Template view
+type TemplateView struct {
+	RestView
+}
+
+// Overwrite this method
+// Return your template name
+func (t *TemplateView) GetTemplateName(ctx *gin.Context) string {
+	panic("implement me")
+}
+
+// Set template Context
+func (t *TemplateView) GetTemplateContext(ctx *gin.Context) interface{} {
+	return nil
+}
+
+func (t *TemplateView) Get(ctx *gin.Context) {
+	panic("implement me")
 }
