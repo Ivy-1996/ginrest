84d86ece6917cd14224ae77c83cc072077c46c81
ivy-1996 <919624032@qq.com>
2020-10-16 12:05:47 +0800

    add validate fun support

--- http/generic.go
+++ http/generic.go
@@ -5,10 +5,10 @@ import (
 	"github.com/ivy-1996/ginrest/serializer"
 )
 
-func ValidateHandler(ctx *gin.Context, serializer serializer.Serializer) {
+func ValidateHandler(ctx *gin.Context, serializer serializer.Serializer, i interface{}) {
 	var err error
 	// validate data
-	if err = serializer.Validate(ctx); err == nil {
+	if err = serializer.Validate(ctx, i); err == nil {
 		serializer.OnSuccess(ctx)
 	} else {
 		serializer.OnFail(ctx, err)
--- serializer/serializer.go
+++ serializer/serializer.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 type Serializer interface {
 	// Validate request with this method
-	Validate(ctx *gin.Context) error
+	Validate(ctx *gin.Context, i interface{}) error
 	// OnSuccess will be called if Validate return nil
 	// Handle allow request with this method
 	OnSuccess(ctx *gin.Context)
